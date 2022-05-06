2e997e62d16ad3dec3fbd41fecdf2c354db03a56
melina pazoki <melina.pazoki@tecnotree.com>
2022-05-06 19:07:43 +0430

    feat: add functionality of : show players and the actions related to players

--- src/modules/Players.js
+++ src/modules/Players.js
@@ -1,7 +1,27 @@
-import React from "react";
+import Player from "../components/Player";
+import Grid from "@mui/material/Grid";
 
-const Players = () => {
-	return <div></div>;
+const players = ({
+	playersInfo,
+	handleDiceAction,
+	playerScore,
+	totalScore,
+	nextPlayer,
+}) => {
+	return (
+		<Grid container spacing={8}>
+			{playersInfo.map((player) => (
+				<Player
+					key={player.id}
+					info={player}
+					playerScore={playerScore[player.id]}
+					handleDiceAction={handleDiceAction}
+					totalScore={totalScore}
+					nextPlayer={nextPlayer}
+				/>
+			))}
+		</Grid>
+	);
 };
 
-export default Players;
+export default players;
--- src/pages/MainGame.js
+++ src/pages/MainGame.js
@@ -1,7 +1,61 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-const MainGame = () => {
-	return <div></div>;
+import Players from "../modules/Players";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+
+import Dice from "../components/Dice";
+
+const MainGame = ({ players }) => {
+	const [playerScore, setPlayerScore] = useState({});
+
+	let [{ diceResult, playerId, nextPlayerIndex = 0 }, setRollResult] = useState(
+		{},
+	);
+	useEffect(() => {
+		if (Object.keys(playerScore).length === 0) {
+			players.map((player) => (playerScore[player.id] = 0));
+		}
+		if (playerId) {
+			playerScore[playerId] += diceResult;
+		}
+		setPlayerScore(playerScore);
+	}, [diceResult, playerId, playerScore, nextPlayerIndex]);
+
+	const handleDiceAction = (playerInfo) => {
+		// find a number between 1 - 6
+		const diceResult = Math.floor(Math.random() * 5) + 1;
+		//in each action we know who is next player
+		players[nextPlayerIndex + 1] ? nextPlayerIndex++ : (nextPlayerIndex = 0);
+		setRollResult({
+			diceResult,
+			playerId: playerInfo.id,
+			nextPlayerIndex,
+		});
+	};
+
+	return (
+		<Container sx={{ p: 4 }} maxWidth="lg">
+			<Grid container>
+				<Grid item md={8}>
+					<Dice diceResult={diceResult} />
+				</Grid>
+				<Grid item md={4}>
+					<Typography fontWeight={700}>Dice Game</Typography>
+					<Typography fontWeight={700}>Score to win: {scoreToWin}</Typography>
+					<Typography fontWeight={700}>Match Id: {matchId}</Typography>
+				</Grid>
+				<Players
+					playersInfo={players}
+					handleDiceAction={handleDiceAction}
+					playerScore={playerScore}
+					totalScore={10}
+					nextPlayer={players[nextPlayerIndex]}
+				/>
+			</Grid>
+		</Container>
+	);
 };
 
 export default MainGame;
