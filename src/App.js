8c9711552667a286d70153813a1f29b88ff6394d
melina pazoki <melina.pazoki@tecnotree.com>
2022-05-07 03:21:51 +0430

    feat: final touch up

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import MainGame from "./pages/MainGame";
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<MainGame />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { render } from "react-dom";
-
+import ReactDOM from "react-dom/client";
 import "./index.scss";
+import App from "./App";
 
-const App = () => (
-  <div>
-    <h1>It works!</h1>
-  </div>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 );
-
-render(<App />, document.getElementById("app"));
